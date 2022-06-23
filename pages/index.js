4f2d200b14805ee604d98040e1a400489d8f5035
thinh bui <thinhbuihong.ma@gmail.com>
2022-06-23 19:22:01 +0700

    feat: get videos from google api

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,38 @@
 import Head from 'next/head'
 import Banner from '../components/banner/banner'
-import Card from '../components/card/card'
+import SectionCards from '../components/card/section-cards'
 import NavBar from '../components/nav/navbar'
+import { getPopularVideos, getVideos } from '../lib/videos'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export async function getServerSideProps(context) {
+  // const { userId, token } = await useRedirectUser(context);
+  // const watchItAgainVideos = await getWatchItAgainVideos(userId, token);
+
+  const disneyVideos = await getVideos("disney trailer");
+  const productivityVideos = await getVideos("Productivity");
+
+  const travelVideos = await getVideos("indie music");
+
+  const popularVideos = await getPopularVideos();
+  return {
+    props: {
+      disneyVideos,
+      travelVideos,
+      productivityVideos,
+      popularVideos,
+      // watchItAgainVideos,
+    },
+  };
+}
+
+export default function Home({
+  disneyVideos,
+  travelVideos,
+  productivityVideos,
+  popularVideos,
+  watchItAgainVideos,
+}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -22,7 +50,21 @@ export default function Home() {
         imgUrl="/static/clifford.webp"
       />
 
-      <Card />
+      <div className={styles.sectionWrapper}>
+        <SectionCards title="Disney" videos={disneyVideos} size="large" />
+        <SectionCards
+          title="Watch it again"
+          videos={watchItAgainVideos}
+          size="small"
+        />
+        <SectionCards title="Travel" videos={travelVideos} size="small" />
+        <SectionCards
+          title="Productivity"
+          videos={productivityVideos}
+          size="medium"
+        />
+        <SectionCards title="Popular" videos={popularVideos} size="small" />
+      </div>
     </div>
   )
 }
